123b9ea7dc17ced3b129abdfbfa5bc6a6fe608de
Laguisma, Gian Pierre <99235356+LaguismaGian@users.noreply.github.com>
2026-01-22 14:43:02 +0800

    Update SeondPart.cs

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,7 +36,59 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
         {
             Console.WriteLine("SECOND PART");
 
-            //Put your code here
+            Student student1, student2;
+            
+            student1 = new Student();
+            student1.StudentNumber = "1001";
+            student1.StudentName = "Austin Dovrich";
+            student1.Course = "Nuclear Engineering";
+            student1.YearLevel = 4;
+            student1.Section = 1;
+            student1.Favourite_Filipino_Hero = "Sarah Duterte";
+
+            Console.WriteLine("Student Number:" + student1.StudentNumber);
+            Console.WriteLine("Student Name: " + student1.StudentName);
+            Console.WriteLine("Course: " + student1.Course);
+            Console.WriteLine("Year Level:" + student1.YearLevel);
+            Console.WriteLine("Section: " + student1.Section);
+            Console.WriteLine("Favourite Filipino Hero: " + student1.Favourite_Filipino_Hero);
+
+            Console.WriteLine();
+
+
+            student2 = student1;
+            student2.StudentName = "Maximum Intensity X. Larionova";
+            student2.Favourite_Filipino_Hero = "Melchora 'Tandang Sora' Aquino";
+
+            Console.WriteLine("student 1");
+            Console.WriteLine("Student Number:" + student1.StudentNumber);
+            Console.WriteLine("Student Name: " + student1.StudentName);
+            Console.WriteLine("Course: " + student1.Course);
+            Console.WriteLine("Year Level:" + student1.YearLevel);
+            Console.WriteLine("Section: " + student1.Section);
+            Console.WriteLine("Favourite Filipino Hero: " + student1.Favourite_Filipino_Hero);
+
+            Console.WriteLine();
+            Console.WriteLine("student 2");
+            Console.WriteLine("Student Number:" + student2.StudentNumber);
+            Console.WriteLine("Student Name: " + student2.StudentName);
+            Console.WriteLine("Course: " + student2.Course);
+            Console.WriteLine("Year Level:" + student2.YearLevel);
+            Console.WriteLine("Section: " + student2.Section);
+            Console.WriteLine("Favourite Filipino Hero: " + student2.Favourite_Filipino_Hero);
+
+
+
         }
     }
+    public class Student
+    {
+       public String StudentNumber;
+       public String StudentName;
+       public String Course;
+       public int YearLevel;
+       public int Section;
+       public String Favourite_Filipino_Hero;
+    }
+
 }
