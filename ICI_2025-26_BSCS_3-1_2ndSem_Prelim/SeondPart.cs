7c6602a4910b235760ee2011d7b588482c1e974a
lhoydangeles78-stack <lhoydangeles78@gmail.com>
2026-01-22 14:43:42 +0800

    Implement student details display in SeondPart.cs
    lhoyd2

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ICI_2025_26_BSCS_3_1_2ndSem_Prelim.SeondPart;
+using static System.Collections.Specialized.BitVector32;
 
 namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 {
@@ -36,7 +38,92 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
         {
             Console.WriteLine("SECOND PART");
 
-            //Put your code here
+
+
+            students student1 = new students();
+
+
+
+            student1.StudentNumber = "2025-12345";
+            student1. StudentName = "Juan Dela Cruz";
+            student1. Course = "BSIS";
+            student1. YearLevel = 3;
+            student1. Section = 2;
+            student1. FavoriteFilipinoHero = "Jose Rizal";
+
+
+            Console.WriteLine("Student 1 Details:");
+            Console.WriteLine($"Student Number: {student1.StudentNumber}");
+            Console.WriteLine($"Student Name: {student1.StudentName}");
+            Console.WriteLine($"Course: {student1.Course}");
+            Console.WriteLine($"Year Level: {student1.YearLevel}");
+            Console.WriteLine($"Section: {student1.Section}");
+            Console.WriteLine($"Favorite Filipino Hero: {student1.FavoriteFilipinoHero}");
+            Console.WriteLine();
+
+
+
+            students student2 = new students();
+
+
+
+            Console.WriteLine("Student 1 Details:");
+            Console.WriteLine($"Student Number: {student1.StudentNumber}");
+            Console.WriteLine($"Student Name: {student1.StudentName}");
+            Console.WriteLine($"Course: {student1.Course}");
+            Console.WriteLine($"Year Level: {student1.YearLevel}");
+            Console.WriteLine($"Section: {student1.Section}");
+            Console.WriteLine($"Favorite Filipino Hero: {student1.FavoriteFilipinoHero}");
+            Console.WriteLine();
+       
+
+
+            student2.StudentNumber = "2026-12345";
+            student2.StudentName = "ALING DIANA";
+            student2.Course = "TOURSIM";
+            student2.YearLevel = 3;
+            student2.Section = 2;
+            student2.FavoriteFilipinoHero = "DIWATA PARES OVER LOAD";
+
+
+            students student3 = new students();
+
+
+
+            Console.WriteLine("Student 1 Details:");
+            Console.WriteLine($"Student Number: {student1.StudentNumber}");
+            Console.WriteLine($"Student Name: {student1.StudentName}");
+            Console.WriteLine($"Course: {student1.Course}");
+            Console.WriteLine($"Year Level: {student1.YearLevel}");
+            Console.WriteLine($"Section: {student1.Section}");
+            Console.WriteLine($"Favorite Filipino Hero: {student1.FavoriteFilipinoHero}");
+            Console.WriteLine();
+
+
+            student3.StudentNumber = "2025-12345";
+            student3.StudentName = "LUFFY";
+            student3.Course = "EDUC";
+            student3.YearLevel = 3;
+            student3.Section = 2;
+            student3.FavoriteFilipinoHero = "Andress Bonifacio";
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+        
+
+
+        }
+
+        public class students   
+        {
+          public string StudentNumber;
+          public string StudentName;
+          public string Course;  
+          public int YearLevel;
+          public int Section;
+          public string FavoriteFilipinoHero;
         }
     }
 }
+
