104a56e8fa11f99b779203b012f14aa944cb1e03
chard <chardestavillo@gmail.com>
2026-01-22 14:44:15 +0800

    Add ApplyDamage method to manage health reduction
    Implemented ApplyDamage method to reduce health of Hero and Minion.

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/ThirdPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/ThirdPart.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +47,7 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 
         public void Build()
         {
+            Console.WriteLine("Third Parts");
             // TODO 3: Create instances of both and set Health to 100
             HeroClass myHero = new HeroClass();
             myHero.Health = 100;
@@ -57,6 +58,7 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
             Console.WriteLine($"Start: Hero Health = {myHero.Health}, Minion Health = {myMinion.Health}");
 
             // TODO 4: Call ApplyDamage here
+            ApplyDamage(myHero, myMinion);
             // ____________________
 
             Console.WriteLine($"End:   Hero Health = {myHero.Health}, Minion Health = {myMinion.Health}");
@@ -65,6 +67,21 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
         // TODO 5: Write the ApplyDamage method. 
         // It should take (HeroClass h, MinionStruct m) as parameters.
         // It should subtract 10 from the Health of both.
+
+        public void ApplyDamage(HeroClass h,MinionStruct m)
+        {
+            h.Health  -= 10;
+            m.Health  -= 10;
+        }
         // ____________________
+    }
+    public class  HeroClass
+    {
+        public int Health;
+    }
+
+    public struct MinionStruct
+    {
+        public int Health;
     }
 }
